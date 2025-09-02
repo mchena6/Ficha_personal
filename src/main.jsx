8a1fc0d102ed62d02b6a5756dfcb85a2d7496f6a
mchena <m.chena@itecriocuarto.org.ar>
2025-09-01 21:41:19 -0300

    BrowserRouter y StricMode agregados

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
-import { render } from 'preact'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './app.jsx'
+import App from './app.jsx'
+import 'primereact/resources/themes/lara-light-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css';
+import { BrowserRouter } from 'react-router-dom'
 
-render(<App />, document.getElementById('app'))
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
