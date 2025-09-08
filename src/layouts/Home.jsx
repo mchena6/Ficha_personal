e47ec33b85147cb2176cdc684ff34ef4e0d26bcb
mchena <m.chena@itecriocuarto.org.ar>
2025-09-07 21:25:04 -0300

    Boton para ruta /personas agregado

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "primereact/button";
+import { Fragment } from "react";
 
 const Home = () =>{
     const navigate = useNavigate()
 
     return(
+        <Fragment>
+
         <Button label="Ir al formulario" onClick={()=> navigate('/tarjeta')}/>
+        <Button label="Ver personas" onClick={()=> navigate('/personas')}/>
+        
+        </Fragment>
     )
 }
 
